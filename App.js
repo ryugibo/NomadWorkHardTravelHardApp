6a1ed56de9ff4ff0c29a0640eea2e32ec5701261
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-05-16 21:30:35 +0900

    :memo: #3.4 | Paint To Dos

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {
   View,
   TouchableOpacity,
   TextInput,
+  ScrollView,
 } from "react-native";
 import { useState } from "react";
 import { theme } from "./colors";
@@ -20,10 +21,7 @@ export default function App() {
     if (text === "") {
       return;
     }
-    const newToDos = Object.assign(toDos, {
-      [Date.now()]: { text, work: working },
-    });
-    setToDos(newToDos);
+    setToDos((toDos) => ({ ...toDos, [Date.now()]: { text, work: working } }));
     setText("");
   };
   console.log(toDos);
@@ -54,6 +52,13 @@ export default function App() {
         style={styles.input}
         placeholder={working ? "Add a To Do" : "Where do you want to go?"}
       />
+      <ScrollView>
+        {Object.keys(toDos).map((key) => (
+          <View style={styles.toDo} key={key}>
+            <Text style={styles.toDoText}>{toDos[key].text}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -78,7 +83,15 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     paddingHorizontal: 20,
     borderRadius: 30,
-    marginTop: 20,
+    marginVertical: 20,
     fontSize: 18,
   },
+  toDo: {
+    backgroundColor: theme.grey,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 15,
+  },
+  toDoText: { color: "white", fontSize: 16, fontWeight: "500" },
 });
